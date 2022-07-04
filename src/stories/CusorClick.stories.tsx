3867616117e9c166e0d388c748a45df03a35adba
jzohdi <31998568+jzohdi@users.noreply.github.com>
2022-07-04 17:19:26 -0400

    add cursor click icon

--- src/stories/CusorClick.stories.tsx
+++ src/stories/CusorClick.stories.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Story } from "@storybook/react";
-import PencilBoldIcon from "../icons/PencilBoldIcon";
 import { IconProps } from "../icons/types";
+import CursorClickIcon from "../icons/CursorClickIcon";
 
 export default {
-  title: "Pencil",
-  component: PencilBoldIcon,
+  title: "CusorClick",
+  component: CursorClickIcon,
 };
 
-const Template: Story<IconProps> = (args) => <PencilBoldIcon {...args} />;
+const Template: Story<IconProps> = (args) => <CursorClickIcon {...args} />;
 
 export const DefaultColorAndSize = Template.bind({});
